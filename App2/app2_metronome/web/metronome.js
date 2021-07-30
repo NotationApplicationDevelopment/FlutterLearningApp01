25a83150ae456b16f0b3a7376daa9cacf4a1dac2
けもみみ愛してる <hirohiro_j3cub@yahoo.co.jp>
2021-07-31 00:28:27 +0900

    update Quality and  Function of metronome.

--- App2/app2_metronome/web/metronome.js
+++ App2/app2_metronome/web/metronome.js
@@ -1,15 +1,58 @@
-function delay(n){
-  return new Promise(function(resolve){
-      setTimeout(resolve,n);
+var timer;
+var promise;
+var play = false;
+
+function delay(n) {
+  const startTime = performance.now();
+  const m = Number(n) * 0.001;
+  return new Promise(function (resolve) {
+    timer = setTimeout(resolve, n / 1000 - 30);
+    while ((performance.now() - startTime) < m);
   });
 }
 
-var play = false;
+async function timerStart(intervalMicroseconds) {
+  return promise = new Promise(async function (resolve) {
+    if (play == false) {
+      play = true;
+      while (play) {
+        await delay(intervalMicroseconds);
+        if (play) {
+          postMessage('js:callback');
+        }
+      }
+    }
+  });
+}
 
-onmessage = async function(e) {
-  play = !play;
-  while(play){
-    await delay(250);
-    postMessage('bye');
+async function timerStop() {
+  cleared = false;
+  play = false;
+  clearInterval(timer);
+  await promise;
+  promise = null;
+}
+
+onmessage = async function (e) {
+  var command = e.data['command'];
+  var intervalMicroseconds = e.data['intervalMicroseconds'];
+
+  switch (command) {
+    case 'start':
+      await timerStart(intervalMicroseconds);
+      break;
+
+    case 'stop':
+      if (play == true) {
+        await timerStop();
+      }
+      break;
+
+    case 'reset':
+      if (play == true) {
+        await timerStop();
+        await timerStart(intervalMicroseconds);
+      }
+      break;
   }
 };
